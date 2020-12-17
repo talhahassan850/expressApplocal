5b6387ed7b55e0ecf09f8ead41b3f130b6998272
talhahassan850 <72450291+talhahassan850@users.noreply.github.com>
2020-12-17 13:24:18 +0500

    Update index.js

--- routes/index.js
+++ routes/index.js
@@ -3,8 +3,8 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
-  //res.redirect('/product');
+  //res.render('index', { title: 'Express' });
+  res.redirect('/product');
   //res.send("hello");
 });
 
