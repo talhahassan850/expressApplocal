a887bc9883bd52997885eef82ec7744119be4561
talhahassan850 <72450291+talhahassan850@users.noreply.github.com>
2020-12-17 13:24:15 +0500

    Update app.js

--- app.js
+++ app.js
@@ -41,7 +41,7 @@ app.use(function(err, req, res, next) {
   res.status(err.status || 500);
   res.render('error');
 });
-
+//expressapp
 mongoose
   .connect(
     "mongodb://talhahassan:talhahassan@cluster0-shard-00-00.gweu2.mongodb.net:27017,cluster0-shard-00-01.gweu2.mongodb.net:27017,cluster0-shard-00-02.gweu2.mongodb.net:27017/A1?ssl=true&replicaSet=atlas-t8bqss-shard-0&authSource=admin&retryWrites=true&w=majority",
